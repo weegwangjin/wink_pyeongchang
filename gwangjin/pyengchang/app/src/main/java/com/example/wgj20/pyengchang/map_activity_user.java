d90b9cf54cf6fa591ec0c54894cf394fa75001fd
Doeun <doeuncow@naver.com>
2017-08-23 21:09:35 +0900

    0823layout

---
+++ gwangjin/pyengchang/app/src/main/java/com/example/wgj20/pyengchang/map_activity_user.java
@@ -0,0 +1,266 @@
+package com.example.wgj20.pyengchang;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class map_activity_user extends AppCompatActivity implements OnMapReadyCallback {
+
+    static final LatLng SEOUL = new LatLng(37.58,127.02);
+    private GoogleMap googleMap;
+
+    private static String TAG = "map_activity_TAG";
+    private static final String TAG_JSON="child";
+    private static final String TAG_UUID = "UUID";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_AGE ="age";
+    private static final String TAG_phonenumber="phonenumber";
+    private static final String TAG_LATITUDE = "latitude";
+    private static final String TAG_LONGITUDE = "longitude";
+
+    String parentID;
+    ListAdapter adapter;
+    ListView mlistView;
+    private TextView mTextViewResult;
+    ArrayList<HashMap<String, String>> mArrayList;
+    String mJsonString;
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        googleMap = map;
+        googleMap.addMarker(new MarkerOptions().position(SEOUL).title("Seoul"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 17.0f));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_activity);
+
+        Intent intent = getIntent();
+
+        parentID = intent.getStringExtra("ID");
+
+
+        Log.v("parentIDCHECK",parentID);
+        Button myChildInfo = (Button) findViewById(R.id.myChildInfo);
+        myChildInfo.setOnClickListener(mClickListener);
+        mArrayList = new ArrayList<>();
+        mlistView = (ListView) findViewById(R.id.list);
+        Log.d("map_activity","Init GoogleMap");
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.googleMap);
+        mapFragment.getMapAsync(this);
+
+
+        callAsynchronousTask();
+
+        mlistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                  Intent intent = new Intent(map_activity_user.this,child_info.class);
+
+                intent.putExtra("name", mArrayList.get(position).get("name"));
+                intent.putExtra("age",mArrayList.get(position).get("age"));
+                intent.putExtra("phonenumber", mArrayList.get(position).get("phonenumber"));
+                startActivity(intent);
+
+            }
+        });
+
+    }
+
+    Button.OnClickListener mClickListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            Intent intent = new Intent(map_activity_user.this, MyChildActivity.class);
+            intent.putExtra("parentID", parentID);
+            startActivity(intent);
+        }
+    };
+
+    private class GetData extends AsyncTask<String, Void, String> {
+        ProgressDialog progressDialog;
+        String errorString = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(map_activity_user.this,
+                    "Please Wait", null, true, true);
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+            //mTextViewResult.setText(result);
+            Log.d(TAG, "response  - " + result);
+
+            if (result == null){
+
+                mTextViewResult.setText(errorString);
+            }
+            else {
+
+                mJsonString = result;
+                showResult();
+            }
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String serverURL = params[0];
+
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.connect();
+
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+
+                bufferedReader.close();
+
+
+                return sb.toString().trim();
+
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "InsertData: Error ", e);
+                errorString = e.toString();
+
+                return null;
+            }
+
+        }
+    }
+
+
+    private void showResult(){
+        try {
+            JSONObject jsonObject = new JSONObject(mJsonString);
+
+            JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
+            mArrayList = new ArrayList<>();
+            for(int i=0;i<jsonArray.length();i++){
+
+                JSONObject item = jsonArray.getJSONObject(i);
+
+
+                String name = item.getString(TAG_NAME);
+                String phonenumber = item.getString(TAG_phonenumber);
+                String age = item.getString(TAG_AGE);
+
+                HashMap<String,String> hashMap = new HashMap<>();
+
+
+                hashMap.put(TAG_NAME, name);
+                hashMap.put(TAG_AGE, age);
+                hashMap.put(TAG_phonenumber, phonenumber);
+
+                mArrayList.add(hashMap);
+            }
+
+            adapter = new SimpleAdapter(
+                    map_activity_user.this, mArrayList, R.layout.item_list,
+                    new String[]{TAG_NAME,TAG_AGE, TAG_phonenumber},
+                    new int[]{R.id.textView_list_name, R.id.textView_list_age, R.id.textView_list_phonenumber}
+            );
+
+            mlistView.setAdapter(adapter);
+
+        } catch (JSONException e) {
+
+            Log.d(TAG, "showResult : ", e);
+        }
+
+    }
+
+    public void callAsynchronousTask() {
+        final Handler handler = new Handler();
+        Timer timer = new Timer();
+        TimerTask doAsynchronousTask = new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    public void run() {
+                        try {
+                            new GetData().execute("http://13.124.182.10/missingChild.php");
+
+                        } catch (Exception e) {
+                            // TODO Auto-generated catch block
+                        }
+                    }
+                });
+            }
+        };
+        timer.schedule(doAsynchronousTask, 0, 1000); //execute in every 50000 ms
+    }
+
+}
